fa05c650bb9b65884cfb1ebf9841e7052cec3465
beer-psi <lacvtg.a1.2023@gmail.com>
2026-01-01 12:31:48 +0700

    Release 0.3.13

--- docs/kt-chunithm-site-importer.user.js
+++ docs/kt-chunithm-site-importer.user.js
@@ -2,7 +2,7 @@
 /* eslint-disable camelcase */
 // ==UserScript==
 // @name	   kt-chunithm-site-importer
-// @version  0.3.12
+// @version  0.3.13
 // @grant    GM.xmlHttpRequest
 // @connect  kamaitachi.xyz
 // @connect  kamai.tachi.ac
