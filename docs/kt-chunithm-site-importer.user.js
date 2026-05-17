ae64e25189dc48c430422cff6f0af92e3486295f
beer-psi <92439990+beer-psi@users.noreply.github.com>
2026-05-17 09:28:21 +0700

    Release 0.3.14

--- docs/kt-chunithm-site-importer.user.js
+++ docs/kt-chunithm-site-importer.user.js
@@ -2,7 +2,7 @@
 /* eslint-disable camelcase */
 // ==UserScript==
 // @name	   kt-chunithm-site-importer
-// @version  0.3.13
+// @version  0.3.14
 // @grant    GM.xmlHttpRequest
 // @connect  kamaitachi.xyz
 // @connect  kamai.tachi.ac
